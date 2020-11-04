9713c1012130feb54bb715cdad3cb28b9e7c53fb
Vajda Krisztián <uzleti02@gmail.com>
2020-11-04 23:42:55 +0100

    Added Colliders and Collide Script

--- UnityTogether/Assets/Scripts/PlayerMove.cs
+++ UnityTogether/Assets/Scripts/PlayerMove.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : MonoBehaviour
+{
 
- public float moveSpeed;
+    public float moveSpeed;
+    private int count;
+    private Rigidbody Rb;
 
- // Use this for initialization
- void Start () 
- {
-  moveSpeed = 10f;
- }
- 
- // Update is called once per frame
- void Update () 
- {
-  transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime); 
- }
+    // Use this for initialization
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+        moveSpeed = 10f;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
 }
